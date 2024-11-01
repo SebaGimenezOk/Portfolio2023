d0501edb788ed665c957470b4ea8478bed5541aa
Seba Gimenez <sebagimenez@Mac.fibertel.com.ar>
2024-11-01 01:33:58 -0300

    moficaciones simples

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GoBookmark } from "react-icons/go";
 import { GoChevronRight } from "react-icons/go";
-import fotoPerfil from "../assets/para perfil 23.png";
+
 import { Link } from 'react-scroll';
 
 const Home = () => {
@@ -9,8 +9,10 @@ const Home = () => {
         <div name='inicio' className='md:h-screen  w-full  bg-gradient-to-t from-black to-slate-900'>
             <div className='max-w-screen-lg mx-auto py-10 flex flex-col items-center justify-center h-full  px-4  md:flex-row'>
                 <div className='flex flex-col justify-center h-full  '>
-                    <h3 className='text-4xl sm:text-5xl font-semibold text-gray-300 my-3'  >
-                        Hola! Soy Sebastian, programador de apps web y moviles y analista de datos.  <GoBookmark size={80} className='my-6' /> </h3>
+                    <h3 className='text-4xl sm:text-5xl font-semibold text-gray-300 my-3 mb-2
+                    '  >
+                        Desarrollador FrontEnd </h3>
+                        <h3 className='text-4xl sm:text-5xl font-semibold text-gray-300 my-3 mb-2'> Web & mobile. <GoBookmark size={80} className='my-6' /></h3>
                     <p className='text-gray-400'>
                         Este sitio esta hecho con NodeJs, React and Tailwind.
                     </p>
@@ -23,9 +25,7 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div>
-                    <img src={fotoPerfil} alt="my archivo" className=" w-10/12 rounded-2xl mx-auto  md:w-1/2" />
-                </div>
+                
             </div>
         </div>
     )
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import fotoPerfil from "../assets/para perfil 23.png"
+
+
 
 const SobreMi = () => {
     return (
         <div name="sobre mi" className='w-full h-full bg-gradient-to-b from-black to-slate-900 text-white'>
-            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full'>
-                <div className=' pb-5'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center  w-full'>
+                <div className=' pb-4 mb-6 '>
                     <p className='text-5xl capitalize font-bold inline border-b-4 border-gray-500'>sobre mi</p>
                 </div>
+                <div className='flex justify-between'>
+                    
+                    <div >
+                        <p class="mb-3">Desarrollador de aplicaciones web y móviles con experiencia en análisis de datos.</p>
+                        <p className='mb-3'> Formación en Power BI, SQL Server, Excel y Google Sheets.</p>
+                        <p className='mb-3'> Experiencia en React Native, Expo Go y Redux para aplicaciones móviles; React, TypeScript y JavaScript para desarrollo web.</p>
+                        <p className='mb-3'> Conocimientos en NodeJS, Express, MySQL y MongoDB.</p>
+                        <p className='mb-3'> Competencia en HTML5, CSS3, SASS y frameworks como Bootstrap y Material UI.</p>
+                        <p className='mb-3'> Enfoque en soluciones funcionales, optimización de procesos y colaboración en entornos ágiles.</p>
+                        <p className='py-6 mb-6 '>Espero que podamos crear proyectos juntos! Abrazo, Sebastian </p>
+                    </div>
+                    <div >
+                        <img src={fotoPerfil} alt="my archivo" className=" rounded-2xl mx-auto  md:w-6/12 " />
+                    </div>
+                </div>
 
-
-                <p class="mb-4">Desarrollador de aplicaciones web y móviles con experiencia en análisis de datos. Formación en Power BI, SQL Server, Excel y Google Sheets. Experiencia en React Native, Expo Go y Redux para aplicaciones móviles; React, TypeScript y JavaScript para desarrollo web. Conocimientos en NodeJS, Express, MySQL y MongoDB. Competencia en HTML5, CSS3, SASS y frameworks como Bootstrap y Material UI. Enfoque en soluciones funcionales, optimización de procesos y colaboración en entornos ágiles.</p>
-
-
-                <p className='py-6 '>Espero que podamos crear proyectos juntos! Abrazo, Sebastian </p>
             </div>
+
         </div>
     )
 }
