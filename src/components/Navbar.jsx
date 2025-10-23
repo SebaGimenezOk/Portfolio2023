7c945415acf6dfe75b21c58bb3c513308e4cd94b
Seba Gimenez <sebagimenez@MacBook-Air-de-Seba.local>
2025-10-23 17:09:29 -0300

    ajustes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,80 @@
-import React from 'react'
-import { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-scroll';
-
+import React from "react";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false);
+  const links = [
+    {
+      id: 1,
+      link: "inicio",
+    },
+    {
+      id: 2,
+      link: "sobre mi",
+    },
+    {
+      id: 3,
+      link: "skills",
+    },
+    {
+      id: 4,
+      link: "proyectos",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-gradient-to-t from-slate-950 to-slate-900 fixed z-20">
+      <div className="flex items-center">
+        {/* <img src="/logoceleste300.png" alt=""  className="w-22 h-20 object-cover" /> */}
+        <h1 className="text-5xl font-signature  mx-3"> Sebastian Gimenez</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-125 duration-200"
+          >
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-    const links = [
-        {
-            id: 1,
-            link: 'inicio',
-        },
-        {
-            id: 2,
-            link: 'sobre mi',
-        },
-        {
-            id: 3,
-            link: 'skills',
-        },
-        {
-            id: 4,
-            link: 'proyectos',
-        },
-        {
-            id: 5,
-            link: 'contact',
-        },
-    ];
-    return (
-        <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-gradient-to-t from-slate-950 to-slate-900 fixed z-20'>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-white md:hidden"
+      >
+        {nav ? <FaTimes size={18} /> : <FaBars size={18} />}
+      </div>
 
-            <div className='flex items-center'>
-            <img
-                src="/logoconfondo.png"
-                className="w-22 h-20 object-cover"
-            />
-                <h1 className='text-5xl font-signature  mx-3'> Sebastian Gimenez</h1>
-            </div>
-            <ul className='hidden md:flex'>
-                {links.map(({ id, link }) => (
-                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-white hover:scale-125 duration-200'><Link to={link} smooth duration={500}>{link}</Link></li>
-                ))}
-            </ul>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b bg-slate-900  to-gray-800 text-gray-200  ">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-4 text-1xl"
+            >
+              <Link
+                onClick={() => setNav(!nav)}
+                to={link}
+                smooth
+                duration={700}
+              >
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-
-            <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-white md:hidden'>
-                {nav ? <FaTimes size={18} /> : <FaBars size={18} />}
-            </div>
-
-            {nav && (
-                <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b bg-slate-900  to-gray-800 text-gray-200  '>
-                    {links.map(({ id, link }) => (
-                        <li key={id} className='px-4 cursor-pointer capitalize py-4 text-1xl'><Link onClick={() => setNav(!nav)} to={link} smooth duration={700}>{link}</Link></li>
-                    ))}
-                </ul>
-            )}
-
-
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
